8fd24faaa50ba4fc0911b6bf3f65a599951a61bc
Trocolao <daw13.2023@gmail.com>
2023-04-14 13:07:37 +0200

    Carlos Muñoz Glided Rose version 3

--- GlidedRose/Tienda.cs
+++ GlidedRose/Tienda.cs
@@ -11,9 +11,9 @@ namespace GlidedRose
         {
             this.Items = Items;
         }
-        public bool EsMagico(Item it)
+        public bool EsMagico(Item item)
         {
-            string nombre = it.Name.ToLower();
+            string nombre = item.Name.ToLower();
             if (nombre.Contains("conjured"))
             {
                 return true;
@@ -49,28 +49,25 @@ namespace GlidedRose
         {
             if (Items[i].SellIn < 0)
             {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - bajadaCalidad;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
+                if(Items[i].Name == "Aged Brie")
                 {
                     if (Items[i].Quality < 50)
                     {
                         Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+                else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                }
+                else
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].Quality = Items[i].Quality - bajadaCalidad;
+                        }
                     }
                 }
             }
@@ -86,7 +83,15 @@ namespace GlidedRose
 
         private void ActualizarCalidadConSellInPositivo(int i, int bajadaCalidad)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                    ActualizarCalidadDeUnConciertoReciente(i);
+                }
+            }
+            else
             {
                 if (Items[i].Quality > 0)
                 {
@@ -96,34 +101,27 @@ namespace GlidedRose
                     }
                 }
             }
-            else
+        }
+
+        private void ActualizarCalidadDeUnConciertoReciente(int i)
+        {
+            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].Quality < 50)
+                if (Items[i].SellIn < 11)
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (Items[i].Quality < 50)
                     {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+                if (Items[i].SellIn < 6)
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
                     }
                 }
             }
         }
     }
 }
-
-
